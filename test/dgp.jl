cad25e5c44621d6f65b35d10dbe5baa76d3d5ee4
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2024-02-22 21:58:48 -0500

    Composed expressions need sign propogation in test:

--- test/dgp.jl
+++ test/dgp.jl
@@ -15,11 +15,11 @@ ex = SymbolicAnalysis.propagate_gcurvature(ex)
 
 SymbolicAnalysis.getgcurvature(ex)
 
-using Convex
+# using Convex
 
-X = Convex.Variable(5, 5)
-ex = logdet(A'*X*A) - logdet(X)
-vexity(ex)
+# X = Convex.Variable(5, 5)
+# ex = logdet(A'*X*A) - logdet(X)
+# vexity(ex)
 
 ## Karcher Mean
 As = [rand(5,5) for i in 1:5]
@@ -31,6 +31,7 @@ ex = SymbolicAnalysis.propagate_gcurvature(ex)
 SymbolicAnalysis.getgcurvature(ex)
 
 ex = sum(SymbolicAnalysis.sdivergence(X, As[i]) for i in 1:5) |> unwrap
+ex = SymbolicAnalysis.propagate_sign(ex)
 ex = SymbolicAnalysis.propagate_gcurvature(ex)
 
 SymbolicAnalysis.getgcurvature(ex)
